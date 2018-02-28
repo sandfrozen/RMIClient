89ccf57031b39cafe95985f4eadbdd6b44cfa297
Tomek Buslowski <tbuslowski@gmail.com>
2018-02-28 22:38:47 +0100

    Added class Product, products list query and search by name query UI

--- src/rmiclient/RMIClientMain.java
+++ src/rmiclient/RMIClientMain.java
@@ -10,8 +10,10 @@ package rmiclient;
  * @author tomek.buslowski
  */
 import java.rmi.Naming;
+import java.util.List;
 import java.util.Scanner;
 import rmiserver.MyServerInt;
+import rmiserver.Product;
 
 public class RMIClientMain {
 
@@ -22,49 +24,63 @@ public class RMIClientMain {
 
         try {
 
-            MyServerInt myRemoteObject = (MyServerInt) Naming.lookup("//localhost/ABC");
+            MyServerInt myRemoteObject = (MyServerInt) Naming.lookup("//192.168.1.103/ABC");
             String text = "Start :-)";
             String result = myRemoteObject.getDescription(text);
             System.out.println("Wysłano do servera: " + text);
             System.out.println("Otrzymana z serwera odpowiedź: " + result);
-            
+            System.out.println("h - lista obsługiwanych komend");
             Scanner in = new Scanner(System.in);
-            do {
-                System.out.printf("Oblicz:  ");
+            
+            while(true) {
+                System.out.printf("Komenda:  ");
                 text = in.nextLine();
-                result = myRemoteObject.calculator(text);
-                System.out.println("Wynik:   " + result);
-                
-            } while ( !"end".equals(text) );
+                if ( text.startsWith("h") ){
+                    printCommandList();
+                    
+                } else if (text.startsWith("e")){
+                    break;
+                    
+                } else if ( !(text.length() > 1) ){
+                    // nic
+                    
+                } else if ( text.startsWith("c") ){
+                    result = myRemoteObject.calculator(text.substring(2));
+                    System.out.println("Wynik:   " + result);
+                    
+                } else if ( text.startsWith("lp") ){
+                    List<Product> products = myRemoteObject.productsList();
+                    System.out.println("Liczba produktów: " + products.size());
+                    products.forEach((_p) -> {
+                        System.out.println(_p.toString());
+                    });
+                    
+                } else if ( text.startsWith("q") ){
+                    Product product = myRemoteObject.productByName(text.substring(2));
+                    if( product != null ) {
+                        System.out.println("Znaleniono produkt:");
+                        System.out.println(product.toString());
+                    } else {
+                        System.out.println("Nie znaleniono produktu");
+                    }
+                    
+                } else {
+                    System.out.println("Nieobsługiwana komenda");
+                }
+            }
             
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-}
+    private static void printCommandList() {
+        System.out.println("Lista obsługiwanych komend:");
+        System.out.println("    calculator [wyrażenie matematyczne] operacja matematyczna np: c 1+2");
+        System.out.println("    lp                                  lista produktów");
+        System.out.println("    query [nazwa produktu]              szcegóły produktu     np: q iPhone X");
+        System.out.println("    help                                lista komend");
+        System.out.println("    exit                                koniec");
+    }
 
-//
-//                System.out.printf("First number:  ");
-//                a = in.nextDouble();
-//                in.nextLine();
-//                System.out.printf("Calc operation:  ");
-//                op = in.nextLine();
-//                System.out.printf("Sec number:  ");
-//                b = in.nextDouble();
-//                in.nextLine();
-//                switch(op) {
-//                    case "+":
-//                        r = myRemoteObject.sum(a, b);
-//                        break;
-//                    case "-":
-//                        r = myRemoteObject.subtract(a, b);
-//                        break;
-//                    case "*":
-//                        r = myRemoteObject.multiply(a, b);
-//                        break;
-//                    case "/":
-//                        r = myRemoteObject.divide(a, b);
-//                        break;
-//                }
-//                System.out.println("Result: " + a + " " + op + " " + b + " = " + r);
+}
